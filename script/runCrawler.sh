d94910567e049a036a002ec240de30531167e51f
Kien Pham <kienpt.vie@gmail.com>
2014-10-02 22:25:01 -0400

    reduce the default heapsize: 1GB.

--- script/runCrawler.sh
+++ script/runCrawler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd .;
-java -Xmx16g -cp class focusedCrawler.crawler.CrawlerManager conf/crawler/crawler.cfg > log/crawler.log 2>&1 &
+java -Xmx1g -cp class focusedCrawler.crawler.CrawlerManager conf/crawler/crawler.cfg > log/crawler.log 2>&1 &
--- script/runTargetStorage.sh
+++ script/runTargetStorage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd .;
-java -Xmx16g -cp "class/:libs/weka.jar:libs/langdetect.jar:libs/xerces-2.9.1.jar:libs/nekohtml.jar:libs/jsonic-1.2.0.jar:libs/boilerpipe-1.2.0.jar" focusedCrawler.target.TargetStorage conf/target_storage/target_storage.cfg > log/target_storage.log 2>&1 &
+java -Xmx1g -cp "class/:libs/weka.jar:libs/langdetect.jar:libs/xerces-2.9.1.jar:libs/nekohtml.jar:libs/jsonic-1.2.0.jar:libs/boilerpipe-1.2.0.jar" focusedCrawler.target.TargetStorage conf/target_storage/target_storage.cfg > log/target_storage.log 2>&1 &
