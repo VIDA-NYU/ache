4960c911f889fca89a278a090d0c062459d10c6c
Kien Pham <kienpham@gray16.poly.edu>
2014-12-02 22:47:23 -0500

    parameterize scripts

--- script/insert_seeds.sh
+++ script/insert_seeds.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-cd .;
-
-java -cp class:libs/je-3.3.75.jar focusedCrawler.link.frontier.AddSeeds conf/link_storage/link_storage.cfg
+java -cp class:libs/je-3.3.75.jar focusedCrawler.link.frontier.AddSeeds $1 $2
--- script/start_crawler.sh
+++ script/start_crawler.sh
@@ -1,9 +1,19 @@
 mkdir -p log
+if [ $# -eq 3 ]
+then
+    CONFIG_PATH=$1
+    SEED_PATH=$2
+    MODEL_PATH=$3
+else
+    CONFIG_PATH='conf/'
+    SEED_PATH='conf/seeds/ht.seeds'
+    MODEL_PATH='conf/models/ht'
+fi
 sh script/clean_data.sh .
-sh script/insert_seeds.sh
-sh script/run_link_storage.sh
-sh script/run_target_storage.sh conf/ conf/models/ht
-sh script/run_client.sh
+sh script/insert_seeds.sh $CONFIG_PATH $SEED_PATH
+sh script/run_link_storage.sh $CONFIG_PATH $SEED_PATH
+sh script/run_target_storage.sh $CONFIG_PATH $MODEL_PATH
+sh script/run_client.sh 
 sh script/run_client.sh
 sh script/run_client.sh
 sh script/run_client.sh
