735e9fef1766262487cc004b7077a1f2e8100bf0
Kien Pham <kienpham@gray16.poly.edu>
2014-12-03 04:01:38 -0500

    update building model script

--- script/build_model.sh
+++ script/build_model.sh
@@ -1,7 +1,8 @@
 DIRECTORY=$1 #Directory that contain training examples. it should have two subdirectories: positive and negative
 OUTPUT=$2 #output directory
+mkdir -p $OUTPUT
 java -cp "class/:libs/weka.jar" focusedCrawler.target.CreateWekaInput conf/target_storage/target_storage.cfg ${DIRECTORY} ${DIRECTORY}/weka.arff
 java -cp libs/weka.jar weka.classifiers.functions.SMO -M -d ${OUTPUT}/pageclassifier.model -t ${DIRECTORY}/weka.arff
-echo "CLASS_VALUES  S NS\n" > ${OUTPUT}/pageclassifier.features
+echo "CLASS_VALUES  S NS" > ${OUTPUT}/pageclassifier.features
 echo -n "ATTRIBUTES " >> ${OUTPUT}/pageclassifier.features
 cat ${DIRECTORY}/weka.arff | awk '{FS=" "; if (($1=="@ATTRIBUTE") && ($3=="REAL")) {print $2}}' | sed ':a;N;$!ba;s/\n/ /g' >> ${OUTPUT}/pageclassifier.features
