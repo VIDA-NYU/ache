2dc88e34da80366313703197251ee899c6123b41
Kien Pham <kienpham@gray17.poly.edu>
2015-01-09 22:45:07 -0500

    update language detector. Use both lang tag and title to determine language

--- src/focusedCrawler/util/LangDetection.java
+++ src/focusedCrawler/util/LangDetection.java
@@ -12,66 +12,122 @@ import java.util.regex.Pattern;
 public class LangDetection {
     private String[] extList = {".de", ".vn"};
     private int maxHeaderSize;
-    private Pattern searchPattern;
+    private Pattern langPattern;
+    private Pattern titlePattern;
     public LangDetection(){
-      //this.extList = {".de", ".vn"};
-      maxHeaderSize = 10;
-      searchPattern = Pattern.compile("lang=\"(..)\"");
+      maxHeaderSize = 20;
+      langPattern = Pattern.compile("lang=\"(.*?)\"");
+      titlePattern = Pattern.compile("\\<title>(.*)\\</title>", Pattern.CASE_INSENSITIVE|Pattern.DOTALL);      
     }
+
+    public String get_title(String source){
+        String title = null;
+        Matcher title_matcher = this.titlePattern.matcher(source);
+        if (title_matcher.find()){
+            title = title_matcher.group(1);
+        }
+        return title;
+    }
+
 
     public void init(String profileDirectory){
       try{
         DetectorFactory.loadProfile(profileDirectory);
       }
-      catch(Exception e)
-      {
+      catch(Exception e){
         System.out.println("Error in detect language");
       }
     }
 
-    public String detectByBody(String text){
-      try
-      {
-        Detector detector = DetectorFactory.create();
-        detector.append(text);
-        return detector.detect();
-      }
-      catch(Exception e)
-      {
-        System.out.println("Error in detect language");
-        return null;
-      }
-    }
 
-    public Boolean detectByHeader(String text)
+    public Boolean detectByHeader(String source)
     {
+      //Return False if the page is not english
+      int max = 5000;
+      if (source.length() < max)
+        max = source.length();
+      String head = source.substring(0, max);
+      //System.out.println("NUMBER OF LINES: " + head.split("\n").length);
       try
       {
-        BufferedReader bufReader = new BufferedReader(new StringReader(text));
-        String line=null;
-        int count = 0;
-        while( (line=bufReader.readLine()) != null )
+        //Extract the tag lang in the header of the page. Check if this tag is 'en'
+        Matcher lang_match = langPattern.matcher(head);
+        if(lang_match.find())
         {
-          Matcher m = searchPattern.matcher(line);
-          if(m.find())
-          {
-            //System.out.println(m.group(1));
-            if (m.group(1).toLowerCase().equals("en"))
-            {
-              return true;
-            }
-            else
-            {
-              return false;
-            }
+          //System.out.println(m.group(1));
+          if (lang_match.group(1).toLowerCase().substring(0, 2).equals("en")){
+            return true;
           }
-          count += 1;
-          if (count == maxHeaderSize)
-            break;
-        }   
+          else{ 
+            System.out.println("CODE: " + lang_match.group(1));
+            return false;
+          }
+        }
+
         return true;
-      } catch (Exception ex)
+      } catch (Exception ex){
+        ex.printStackTrace();
+        System.out.println(">>>>Exception in language detection");
+        return false;
+      }
+
+    }
+
+    public Boolean detectByHeaderAndTitle(String source)
+    {
+      //Return False if the page is not english
+      int max = 5000;
+      if (source.length() < max)
+        max = source.length();
+      String head = source.substring(0, max);
+      //System.out.println("NUMBER OF LINES: " + head.split("\n").length);
+      try
       {
+        //Extract the tag lang in the header of the page. Check if this tag is 'en'
+        Matcher lang_match = langPattern.matcher(head);
+        if(lang_match.find())
+        {
+          //System.out.println(m.group(1));
+          if (lang_match.group(1).toLowerCase().substring(0, 2).equals("en")){
+            return true;
+          }
+          else{ 
+            return false;
+          }
+        }
+
+        //If can not extract tag lang, extract the title then check if title is in English
+        //Note that title is usually short, therefore the detection is prone to error. However extracting text is expensive 
+        String title = get_title(head);
+        if (title != null){
+            if (title.split(" ").length < 20)//langdetect does not work correctly if text is small
+                return true;
+            Detector detector = DetectorFactory.create();
+            detector.append(title);
+            ArrayList<Language> langs = detector.getProbabilities();
+            if (langs.size() == 0){
+                System.out.println("TITLE: " + title);
+                return false;
+            }
+            for (Language l: langs){
+                if (l.lang.equals("en"))
+                    return true;
+            }
+            System.out.println("TITLE: " + title);
+            return false;
+            /*
+            ArrayList<Language> langs = detector.detect();
+            String lang = detector.detect();
+            if (lang.equals("en")){
+                return true;
+            }
+            else{
+                return false;
+            }*/
+        }
+        //If both title and lang tag can not be extracted, assume this page is in English
+        return true;
+      } catch (Exception ex){
         ex.printStackTrace();
         System.out.println(">>>>Exception in language detection");
         return false;
@@ -80,12 +136,9 @@ public class LangDetection {
 
     public Boolean detect_page(Page page){
       //Return False if the page is not Enlgish
-      //Check the url extension:
-      //if (check_ext(page.getURL().getHost())){
       try{
-        //String text = ArticleExtractor.INSTANCE.getText(page.getContent());
-        String text = page.getContent();
-        if (detectByHeader(text)){
+        String source = page.getContent();
+        if (detectByHeaderAndTitle(source)){
           return true;
         }
         else{
@@ -96,43 +149,6 @@ public class LangDetection {
         System.out.println("Exception in detect_page");
         return false;
       }
-    }
-    public Boolean check_content(String content)
-    {
-      if (detectByHeader(content).equals("en")){
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-
-    public Boolean check_ext(String url){
-      String ext = url.substring(url.length() - 3);
-      for (int i=0; i<extList.length; i++){
-        if (ext.equals(extList[i])){
-          return false;
-        }
-      }
-      return true;
-    }
-
-    public Boolean check_header(String content){
-      return true;
-    }
-
-    public ArrayList<Language> detectLangs(String text){
-      try
-      {
-        Detector detector = DetectorFactory.create();
-        detector.append(text);
-        return detector.getProbabilities();
-      }
-      catch(Exception e)
-      {
-        System.out.println("Error in detect language");
-        return null;
-      }     
     }
     
     public static void main(String[] args)
