ee1d241698e30945d9b25538b6a5ca78c6e910b6
Kien Pham <kienpham@gray17.poly.edu>
2015-02-26 20:27:28 -0500

    update the script to rerun client

--- script/rerun_clent.sh
+++ script/rerun_clent.sh
@@ -1,6 +1,7 @@
 while true
 do
-  x=$(ps aux | grep CrawlerManager | grep kienpham | wc -l)
+  x=$(ps aux | grep ache | grep kienpham | wc -l) #kienpham should be changed to username of crawler's owner
+                                                  #also, we should change the client so that there is no need to rerun it
   if [ $x != 7 ]
   then
     sh script/run_client.sh
