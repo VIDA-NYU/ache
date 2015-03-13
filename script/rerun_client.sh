1eb0bf8b937e67016b671ec9744e281e956117e3
Kien Pham <kienpt.vie@gmail.com>
2015-03-13 10:48:19 -0400

    Update rerun_client.sh

--- script/rerun_client.sh
+++ script/rerun_client.sh
@@ -2,7 +2,7 @@ while true
 do
   x=$(ps aux | grep ache | grep kienpham | wc -l) #kienpham should be changed to username of crawler's owner
                                                   #also, we should change the client so that there is no need to rerun it
-  if [ $x != 7 ]
+  if [ $x -lt 7 ]
   then
     sh script/run_client.sh config/sample_config
     echo "rerun"
