5367de4d8bad28b923bf1138d633542be53f8d02
Aecio Santos <aecio.solando@gmail.com>
2015-04-30 18:01:53 -0400

    Changed lang detection for a newer version with builtin profiles

---
+++ src/test/java/focusedCrawler/util/LangDetectionTest.java
@@ -0,0 +1,43 @@
+package focusedCrawler.util;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LangDetectionTest {
+    
+    static LangDetection langDetect;
+    
+    @BeforeClass
+    public static  void setUpClass() {
+        langDetect = new LangDetection();
+//        langDetect.init("libs/profiles/");
+    }
+
+    @Test
+    public void shouldDetectEnglishLanguage() {
+        // given
+        String textInEnglish = "This text is written in plain regular english language";
+        
+        // when
+        Boolean isEnglish = langDetect.isEnglish(textInEnglish);
+        
+        // then
+        assertThat(isEnglish, is(true));
+    }
+    
+    @Test
+    public void shouldDetectWhenTextIsNotEnglishLanguage() {
+        // given
+        String textNotInEnglish = "Este texto está escrito em lingua portuguesa.";
+        
+        // when
+        Boolean isEnglish = langDetect.isEnglish(textNotInEnglish);
+        
+        // then
+        assertThat(isEnglish, is(false));
+    }
+
+}
