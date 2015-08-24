c477bf96148add426e20a044d467de09ea35f9dd
Aecio Santos <aecio.solando@gmail.com>
2015-08-24 17:26:19 -0400

    More code formating and warnings removed

--- src/main/java/focusedCrawler/link/classifier/util/Instance.java
+++ src/main/java/focusedCrawler/link/classifier/util/Instance.java
@@ -34,83 +34,82 @@ import java.util.HashMap;
  * @author Luciano Barbosa
  * @version 1.0
  */
-
 public class Instance {
 
-  private HashMap featureHash;
-  private String[] features;
-  private double[] values;
-  private double valueClassification;
+    private HashMap<String, Integer> featureHash;
+    private String[] features;
+    private double[] values;
+    private double valueClassification;
 
-  public Instance(String[] features) {
-    this.setFeatures(features);
-  }
-
-  public void setClassification(String feat, double value){
-    this.valueClassification = value;
-  }
-
-  public void setFeatures(String[] features){
-    this.features = features;
-    values = new double[features.length];
-    featureHash = new HashMap(features.length);
-    for (int i = 0; i < features.length; i++) {
-      featureHash.put(features[i],new Integer(i));
-    }
-  }
-
-  public void setValue(String feat, Double val){
-    Integer index = (Integer)featureHash.get(feat);
-    if(index != null){
-      values[index.intValue()] = val.doubleValue();
-    }
-  }
-
-  public String[] getFeatures(){
-    return features;
-  }
-
-  public double[] getValues(){
-    return values;
-  }
-
-  public HashMap getHash(){
-    return featureHash;
-  }
-
-  public boolean checkFeature(String feat){
-    boolean exist = false;
-    if(featureHash.get(feat) != null){
-      exist = true;
-    }
-    return exist;
-  }
-
-  public String toString(){
-
-    StringBuffer temp = new StringBuffer();
-    for (int i = 0; i < features.length; i++) {
-//      if(values[i] > 0 && features[i].indexOf("text") == -1 && features[i].indexOf("title") == -1){
-
-//        temp.append(features[i]);
-
-//        temp.append(" ");
-
-//      }
-
-      temp.append(features[i]);
-      temp.append(" ");
-
-      temp.append((int)values[i]);
-      temp.append(",");
+    public Instance(String[] features) {
+        this.setFeatures(features);
     }
 
-//    temp.append(attributeClassification);
+    public void setClassification(String feat, double value) {
+        this.valueClassification = value;
+    }
 
-//    temp.append(" ");
-    temp.append((int)valueClassification);
-    return temp.toString();
-  }
+    public void setFeatures(String[] features) {
+        this.features = features;
+        this.values = new double[features.length];
+        this.featureHash = new HashMap<String, Integer>(features.length);
+        for (int i = 0; i < features.length; i++) {
+            this.featureHash.put(features[i], new Integer(i));
+        }
+    }
+
+    public void setValue(String feat, Double val) {
+        Integer index = (Integer) featureHash.get(feat);
+        if (index != null) {
+            values[index.intValue()] = val.doubleValue();
+        }
+    }
+
+    public String[] getFeatures() {
+        return features;
+    }
+
+    public double[] getValues() {
+        return values;
+    }
+
+    public HashMap<String, Integer> getHash() {
+        return featureHash;
+    }
+
+    public boolean checkFeature(String feat) {
+        boolean exist = false;
+        if (featureHash.get(feat) != null) {
+            exist = true;
+        }
+        return exist;
+    }
+
+    public String toString() {
+
+        StringBuffer temp = new StringBuffer();
+        for (int i = 0; i < features.length; i++) {
+            // if(values[i] > 0 && features[i].indexOf("text") == -1 &&
+            // features[i].indexOf("title") == -1){
+
+            // temp.append(features[i]);
+
+            // temp.append(" ");
+
+            // }
+
+            temp.append(features[i]);
+            temp.append(" ");
+
+            temp.append((int) values[i]);
+            temp.append(",");
+        }
+
+        // temp.append(attributeClassification);
+
+        // temp.append(" ");
+        temp.append((int) valueClassification);
+        return temp.toString();
+    }
 
 }
-
