3917225052b7f8216718cace68e3f430ed47bd65
Aecio Santos <aecio.solando@gmail.com>
2015-08-28 20:47:16 -0400

    Fixed AddSeeds

--- src/main/java/focusedCrawler/link/frontier/AddSeeds.java
+++ src/main/java/focusedCrawler/link/frontier/AddSeeds.java
@@ -1,61 +1,50 @@
 package focusedCrawler.link.frontier;
 
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import focusedCrawler.util.LinkRelevance;
+import focusedCrawler.util.ParameterFile;
 import focusedCrawler.util.persistence.PersistentHashtable;
 
 public class AddSeeds {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AddSeeds.class);
 
-	public static void main(String[] args) {
-		try {
+    private static final Logger logger = LoggerFactory.getLogger(AddSeeds.class);
+
+    public static void main(String[] args) {
+        try {
             String linkConfigFile = args[0] + "/link_storage/link_storage.cfg";
             String seedFile = args[1];
             String dataPath = args[2];
-            
-			focusedCrawler.util.ParameterFile config = new focusedCrawler.util.ParameterFile(linkConfigFile);
-			
-			String dir = dataPath + "/" + config.getParam("LINK_DIRECTORY");
-			
-			PersistentHashtable urls = new PersistentHashtable(dir,1000);
-			Frontier frontier = new Frontier(urls, new BaselineLinkSelector(urls));
-			int count = 0;
-            /*
-			if(args.length > 1){
-				BufferedReader input = new BufferedReader(new FileReader(args[1]));
-				for (String line1 = input.readLine(); line1 != null; line1 = input.readLine()) {
-					LinkRelevance linkRel = new LinkRelevance(new URL(line1), 299);
-					frontier.insert(linkRel);
-					count++;
-				}
-			}else{
-				String[] seeds = config.getParam("SEEDS"," ");
-				for (int i = 0; i < seeds.length; i++) {
-					urls.put(seeds[i], "299");
-//					LinkRelevance linkRel = new LinkRelevance(new URL(seeds[i]), 299);
-//					boolean added = frontier.insert(linkRel);
-//					System.out.println(added);
-					count++;
-				}
-			}*/
-            //String[] seeds = seedConfig.getParam("SEEDS"," ");
-            String[] seeds = focusedCrawler.util.ParameterFile.getSeeds(seedFile);
-            for (int i = 0; i < seeds.length; i++) {
-              if(urls.get(seeds[i]) == null) {
-                String relevance = String.valueOf(LinkRelevance.DEFAULT_RELEVANCE);
-                urls.put(seeds[i], relevance);
-                count++;
-              }
+
+            ParameterFile config = new ParameterFile(linkConfigFile);
+            String dir = dataPath + "/" + config.getParam("LINK_DIRECTORY");
+
+            PersistentHashtable urls = new PersistentHashtable(dir, 1000);
+            Frontier frontier = new Frontier(urls, new BaselineLinkSelector(urls));
+
+            int count = 0;
+
+            logger.info("Adding seeds from file: " + seedFile);
+
+            String[] seeds = ParameterFile.getSeeds(seedFile);
+            if (seeds != null && seeds.length > 0) {
+                for (String seed : seeds) {
+                    if (urls.get(seed) == null) {
+                        LinkRelevance linkRel = new LinkRelevance(new URL(seed), LinkRelevance.DEFAULT_RELEVANCE);
+                        frontier.insert(linkRel);
+                        count++;
+                    }
+                }
             }
-			logger.info("Number of seeds added:" + count);
-			frontier.close();
-			
-		} catch (Exception e) {
-			logger.error("Problem while adding seeds. ", e);
-		}
-	}
-	
+            logger.info("Number of seeds added:" + count);
+            frontier.close();
+
+        } catch (Exception e) {
+            logger.error("Problem while adding seeds. ", e);
+        }
+    }
+
 }
