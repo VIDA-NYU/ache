24e443b213e1f598e2a30e9e7cf3d53dbce2fef6
Aecio Santos <aecio.solando@gmail.com>
2015-12-01 23:37:53 -0500

    Store response headers in Page object

--- src/main/java/focusedCrawler/crawler/async/FetchedResultHandler.java
+++ src/main/java/focusedCrawler/crawler/async/FetchedResultHandler.java
@@ -1,7 +1,12 @@
 package focusedCrawler.crawler.async;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.tika.metadata.Metadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,9 +52,12 @@ public class FetchedResultHandler implements HttpDownloader.Callback {
     
     private void processData(FetchedResult response) {
         try {
-            String body = new String(response.getContent());
-            
-            Page page = new Page(new URL(response.getFetchedUrl()), body);
+            Page page = new Page(
+                new URL(response.getBaseUrl()),
+                new String(response.getContent()),
+                parseResponseHeaders(response.getHeaders()),
+                new URL(response.getFetchedUrl())
+            );
             PaginaURL pageParser = new PaginaURL(page.getURL(), page.getContent());
             page.setPageURL(pageParser);
             
@@ -69,6 +77,17 @@ public class FetchedResultHandler implements HttpDownloader.Callback {
         } catch (Exception e) {
             logger.error("Problem while processing data.", e);
         }
+    }
+
+    private Map<String, List<String>> parseResponseHeaders(Metadata headerAsMetadata) {
+        Map<String, List<String>> responseHeaders = new HashMap<>();
+        String[] names = headerAsMetadata.names();
+        if(names != null && names.length > 0) {
+            for(String name : names) {
+                responseHeaders.put(name, Arrays.asList(headerAsMetadata.getValues(name)));
+            }
+        }
+        return responseHeaders;
     }
     
 }
