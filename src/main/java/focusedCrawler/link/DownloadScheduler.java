84dc3f906f2dde558c08c8345d6c9887cb9368e4
Aecio Santos <aecio.solando@gmail.com>
2016-05-25 21:17:50 -0400

    Removed println

--- src/main/java/focusedCrawler/link/DownloadScheduler.java
+++ src/main/java/focusedCrawler/link/DownloadScheduler.java
@@ -141,8 +141,6 @@ public class DownloadScheduler {
             }
         }
         
-        System.out.println("\t"+numberOfLinks.decrementAndGet()+" links in DownloadScheduler");
-        
         return linkRelevance;
     }
     
