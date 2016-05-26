VIDA-NYU/ache

commit ea6551510b95cf3e5a8ab51b745e37fb048f5e77

Removed config of paths from BipartiteBraphRepository

src/main/java/focusedCrawler/link/BipartiteGraphRepository.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
-import focusedCrawler.link.LinkStorageConfig.BiparitieGraphRepConfig;
 import focusedCrawler.util.parser.BackLinkNeighborhood;
 import focusedCrawler.util.parser.LinkNeighborhood;
 import focusedCrawler.util.persistence.PersistentHashtable;
@@ -32,15 +31,18 @@ public class BipartiteGraphRepository {
 	
 	private final String separator = "###";
 	
-	
+    private String urlIdDirectory = "data_backlinks/url";
+    private String authIdDirectory = "data_backlinks/auth_id";
+    private String hubIdDirectory = "data_backlinks/hub_id";
+    private String hubGraphDirectory = "data_backlinks/hub_graph";
 
-    public BipartiteGraphRepository(String dataPath, BiparitieGraphRepConfig config) {
+    public BipartiteGraphRepository(String dataPath) {
         int cacheSize = 10000;
-        this.authGraph = new PersistentHashtable<>(dataPath + "/" + config.getAuthGraphDirectory(), cacheSize, String.class);
-        this.url2id = new PersistentHashtable<>(dataPath + "/" + config.getUrlIdDirectory(), cacheSize, String.class);
-        this.authID = new PersistentHashtable<>(dataPath + "/" + config.getAuthIdDirectory(), cacheSize, String.class);
-        this.hubID = new PersistentHashtable<>(dataPath + "/" + config.getHubIdDirectory(), cacheSize, String.class);
-        this.hubGraph = new PersistentHashtable<>(dataPath + "/" + config.getHubGraphDirectory(), cacheSize, String.class);
+        this.authGraph = new PersistentHashtable<>(dataPath + "/" + "", cacheSize, String.class);
+        this.url2id = new PersistentHashtable<>(dataPath + "/" + urlIdDirectory, cacheSize, String.class);
+        this.authID = new PersistentHashtable<>(dataPath + "/" + authIdDirectory, cacheSize, String.class);
+        this.hubID = new PersistentHashtable<>(dataPath + "/" + hubIdDirectory, cacheSize, String.class);
+        this.hubGraph = new PersistentHashtable<>(dataPath + "/" + hubGraphDirectory, cacheSize, String.class);
     }
 	
 	public Tuple<String>[] getAuthGraph() throws Exception{

src/main/java/focusedCrawler/link/LinkStorage.java
@@ -221,7 +221,7 @@ public class LinkStorage extends StorageDefault{
 
         FrontierManager frontierManager = FrontierManagerFactory.create(config, configPath, dataPath, seedFile, stoplistFile);
 
-        BipartiteGraphRepository graphRep = new BipartiteGraphRepository(dataPath, config.getBiparitieGraphRepConfig());
+        BipartiteGraphRepository graphRep = new BipartiteGraphRepository(dataPath);
 
         BipartiteGraphManager manager = createBipartiteGraphManager(config, linkClassifier, frontierManager, graphRep);
 

src/main/java/focusedCrawler/link/LinkStorageConfig.java
@@ -31,38 +31,6 @@ public class LinkStorageConfig {
         
     }
     
-    public static class BiparitieGraphRepConfig {
-        
-        private String authGraphDirectory = "data_backlinks/auth_graph";
-        private String urlIdDirectory = "data_backlinks/url";
-        private String authIdDirectory = "data_backlinks/auth_id";
-        private String hubIdDirectory = "data_backlinks/hub_id";
-        private String hubGraphDirectory = "data_backlinks/hub_graph";
-        
-        public BiparitieGraphRepConfig() { }
-        
-        public String getAuthGraphDirectory() {
-            return authGraphDirectory;
-        }
-        
-        public String getUrlIdDirectory() {
-            return urlIdDirectory;
-        }
-        
-        public String getAuthIdDirectory() {
-            return authIdDirectory;
-        }
-        
-        public String getHubIdDirectory() {
-            return hubIdDirectory;
-        }
-        
-        public String getHubGraphDirectory() {
-            return hubGraphDirectory;
-        }
-        
-    }
-    
     @JsonProperty("link_storage.max_pages_per_domain")
     private int maxPagesPerDomain = 100;
     
@@ -108,8 +76,6 @@ public class LinkStorageConfig {
     
     @JsonUnwrapped
     private BackSurferConfig backSurferConfig = new BackSurferConfig();
-    
-    private BiparitieGraphRepConfig biparitieGraphRepConfig = new BiparitieGraphRepConfig();
     
     private final StorageConfig serverConfig;
     
@@ -164,10 +130,6 @@ public class LinkStorageConfig {
     
     public String getTargetStorageDirectory() {
         return targetStorageDirectory;
-    }
-    
-    public BiparitieGraphRepConfig getBiparitieGraphRepConfig() {
-        return biparitieGraphRepConfig;
     }
     
     public BackSurferConfig getBackSurferConfig() {

commit 76351bf52b458176bbdcbbd49fe36fb64312573a

Fixed small bug introduced in commit ea6551510b95cf3e5a8ab51b745e37fb048f5e77

src/main/java/focusedCrawler/link/BipartiteGraphRepository.java
@@ -31,6 +31,7 @@ public class BipartiteGraphRepository {
 	
 	private final String separator = "###";
 	
+	private String authGraphDirectory = "data_backlinks/auth_graph";      
     private String urlIdDirectory = "data_backlinks/url";
     private String authIdDirectory = "data_backlinks/auth_id";
     private String hubIdDirectory = "data_backlinks/hub_id";
@@ -38,7 +39,7 @@ public class BipartiteGraphRepository {
 
     public BipartiteGraphRepository(String dataPath) {
         int cacheSize = 10000;
-        this.authGraph = new PersistentHashtable<>(dataPath + "/" + "", cacheSize, String.class);
+        this.authGraph = new PersistentHashtable<>(dataPath + "/" + authGraphDirectory, cacheSize, String.class);
         this.url2id = new PersistentHashtable<>(dataPath + "/" + urlIdDirectory, cacheSize, String.class);
         this.authID = new PersistentHashtable<>(dataPath + "/" + authIdDirectory, cacheSize, String.class);
         this.hubID = new PersistentHashtable<>(dataPath + "/" + hubIdDirectory, cacheSize, String.class);