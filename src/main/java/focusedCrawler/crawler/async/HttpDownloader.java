3752357f7e839d1ec26e6c125b772f20a2a1ff57
Aecio Santos <aecio.solando@gmail.com>
2016-10-04 15:10:12 -0400

    Removed unused imports

--- src/main/java/focusedCrawler/crawler/async/HttpDownloader.java
+++ src/main/java/focusedCrawler/crawler/async/HttpDownloader.java
@@ -13,8 +13,6 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
