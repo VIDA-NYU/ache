3de7279cd85e1322140451197710072baa25a0fd
Aecio Santos <aecio.solando@gmail.com>
2016-10-25 19:41:09 -0400

    Better error logging for cmd line tools

--- src/main/java/focusedCrawler/util/CliTool.java
+++ src/main/java/focusedCrawler/util/CliTool.java
@@ -1,5 +1,6 @@
 package focusedCrawler.util;
 
+import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.ParserProperties;
 
@@ -13,12 +14,15 @@ public abstract class CliTool {
         try {
             parser.parseArgument(args);
             tool.execute();
-        } catch (Exception e) {
+        } catch (CmdLineException e) {
             System.err.println(e.getMessage());
             System.err.println();
             parser.printUsage(System.err);
             System.err.println();
             System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Execution failed: "+e.getMessage());
+            e.printStackTrace(System.err);
         }
     }
     
