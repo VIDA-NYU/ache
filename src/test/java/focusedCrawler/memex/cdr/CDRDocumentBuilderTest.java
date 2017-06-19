3a5c616219902c93525fcbfd15fb81473694c8da
Aecio Santos <aecio.solando@gmail.com>
2017-06-18 20:18:18 -0400

    Added support to CDRv3.1 in AcheToCdrExporter

--- src/test/java/focusedCrawler/memex/cdr/CDRDocumentBuilderTest.java
+++ src/test/java/focusedCrawler/memex/cdr/CDRDocumentBuilderTest.java
@@ -7,12 +7,16 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.ByteStreams;
 
 public class CDRDocumentBuilderTest {
@@ -102,6 +106,61 @@ public class CDRDocumentBuilderTest {
         assertThat(node.get("version").asDouble(), is(3.0d));
         
         
+    }
+
+	@Test
+    public void testSerializeToJsonCDRv31() throws Exception {
+
+        String filename = "http%3A%2F%2Fwww.darpa.mil%2Fprogram%2Fmemex";
+        InputStream fileStream = CDRDocumentBuilderTest.class.getResourceAsStream(filename);
+        String content = new String(ByteStreams.toByteArray(fileStream));
+        Map<String, List<String>> responseHeaders = ImmutableMap.of(
+            "name1", Arrays.asList("value1", "value2"),
+            "name2", Arrays.asList("value3")
+        );
+
+        Date date = new Date(1497763340874l);
+        String json = new CDR31Document.Builder()
+                .setUrl("http://www.darpa.mil/program/memex")
+                .setRawContent(content)
+                .setContentType("text/html")
+                .setCrawler("memex-crawler")
+                .setTeam("DARPA")
+                .setResponseHeaders(responseHeaders)
+                .setTimestampCrawl(date)
+                .setTimestampIndex(date)
+                .buildAsJson();
+
+        JsonNode node = new ObjectMapper().readTree(json);
+
+        assertThat(node.get("url"), is(notNullValue()));
+        assertThat(node.get("url").asText(), is("http://www.darpa.mil/program/memex"));
+
+        assertThat(node.get("raw_content"), is(notNullValue()));
+        assertThat(node.get("raw_content").asText(), is(content));
+
+        assertThat(node.get("response_headers"), is(notNullValue()));
+        assertThat(node.get("response_headers").get("name1").asText(), is("value1,value2"));
+        assertThat(node.get("response_headers").get("name2").asText(), is("value3"));
+
+        assertThat(node.get("content_type"), is(notNullValue()));
+        assertThat(node.get("content_type").asText(), containsString("text/html"));
+
+        assertThat(node.get("crawler"), is(notNullValue()));
+        assertThat(node.get("crawler").asText(), is("memex-crawler"));
+
+        assertThat(node.get("team"), is(notNullValue()));
+        assertThat(node.get("team").asText(), is("DARPA"));
+
+        assertThat(node.get("timestamp_index").asText(), is("2017-06-18T05:22:20Z"));
+        assertThat(node.get("timestamp_crawl").asText(), is("2017-06-18T05:22:20Z"));
+
+        assertThat(node.get("objects"), is(notNullValue()));
+        assertThat(node.get("objects").isArray(), is(true));
+
+        assertThat(node.get("version").asDouble(), is(3.1d));
+
+
     }
 
 }
