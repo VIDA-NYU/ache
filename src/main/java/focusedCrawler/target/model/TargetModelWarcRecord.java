6cf255a38c42e81d91e81cdb365c7c3178867a49
anudeepti2004 <anudeepti2004@gmail.com>
2017-07-06 09:27:37 -0400

    Temp fix

--- src/main/java/focusedCrawler/target/model/TargetModelWarcRecord.java
+++ src/main/java/focusedCrawler/target/model/TargetModelWarcRecord.java
@@ -3,119 +3,155 @@ package focusedCrawler.target.model;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.tika.mime.MediaType;
 import org.archive.format.warc.WARCConstants.WARCRecordType;
 import org.archive.io.warc.WARCRecordInfo;
 
-
+import focusedCrawler.crawler.crawlercommons.fetcher.BaseFetcher;
 import focusedCrawler.target.classifier.TargetRelevance;
 
-
 public class TargetModelWarcRecord extends WARCRecordInfo {
-	
-	private TargetRelevance relevance;
-	private long fetchTime;
-	private String redirectedUrl;
-	private String contentType;
-	private Map<String, List<String>> responseHeaders;
-	
-	public TargetModelWarcRecord() {
-		
-	}
-	
-	public TargetModelWarcRecord(Page page) throws IOException {
-		if (page.getURL() != null)
+
+    private TargetRelevance relevance;
+    private long fetchTime;
+    private String redirectedUrl;
+    private String contentType;
+    private Map<String, List<String>> responseHeaders;
+
+    public TargetModelWarcRecord() {
+    }
+
+    /**
+     * Constructor to convert a Page into a Warc record in order to save as a
+     * Warc data format
+     * 
+     * @param page
+     * @throws IOException
+     */
+    public TargetModelWarcRecord(Page page, URI uri) throws IOException {
+        if (page.getURL() != null)
             this.url = page.getURL().toString();
         if (page.getRedirectedURL() != null)
             this.setRedirectedUrl(page.getRedirectedURL().toString());
         byte[] content = page.getContent();
         this.contentLength = (long) content.length;
-        
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write(content);
-        
-        this.contentStream = new ByteArrayInputStream(baos.toByteArray()); 
+
+        this.contentStream = new ByteArrayInputStream(baos.toByteArray());
         this.setResponseHeaders(page.getResponseHeaders());
         this.setFetchTime(page.getFetchTime());
         this.setContentType(page.getContentType());
 
         this.relevance = page.getTargetRelevance();
         this.type = WARCRecordType.response;
-	}
-	
-	/**
-	 * Sets the target relevance in Warc Record
-	 * @param target
-	 */
-	public void setRelevance(TargetRelevance target) {
-		relevance = target; 
-	}
-	
-	/**
-	 * Returns the relevance of the page
-	 * @return
-	 */
-	public TargetRelevance getRelevance() {
-		return relevance;
-	}
+        this.setMimetype(getMimeTypeFromContentType(page.getContentType()));
+        this.setRecordId(uri);
+    }
+    
+    private  String getMimeTypeFromContentType(String contentType) {
+        String result = "";
+        MediaType mt = MediaType.parse(contentType);
+        if (mt != null) {
+            result = mt.getType() + "/" + mt.getSubtype();
+        }
 
-	/**
-	 * Returns the fetch time of the page
-	 * @return
-	 */
-	public long getFetchTime() {
-		return fetchTime;
-	}
+        return result;
+    }
 
-	/**
-	 * Sets the fetch time for that web page
-	 * @param fetchTime
-	 */
-	public void setFetchTime(long fetchTime) {
-		this.fetchTime = fetchTime;
-	}
-	
+    /**
+     * Sets the target relevance in Warc Record
+     * 
+     * @param target
+     */
+    public void setRelevance(TargetRelevance target) {
+        relevance = target;
+    }
 
-	/**
-	 * Returns the redirected URL
-	 * @return
-	 */
-	public String getRedirectedUrl() {
-		return redirectedUrl;
-	}
+    /**
+     * Returns the relevance of the page
+     * 
+     * @return
+     */
+    public TargetRelevance getRelevance() {
+        return relevance;
+    }
 
-	/**
-	 * Sets the redirected url
-	 * @param redirectedUrl
-	 */
-	public void setRedirectedUrl(String redirectedUrl) {
-		this.redirectedUrl = redirectedUrl;
-	}
+    /**
+     * Returns the fetch time of the page
+     * 
+     * @return
+     */
+    public long getFetchTime() {
+        return fetchTime;
+    }
 
-	/**
-	 * Returns content type
-	 * @return
-	 */
-	public String getContentType() {
-		return contentType;
-	}
+    /**
+     * Sets the fetch time for that web page
+     * 
+     * @param fetchTime
+     */
+    public void setFetchTime(long fetchTime) {
+        this.fetchTime = fetchTime;
+    }
 
-	/**
-	 * Sets the content type of the page
-	 * @param contentType
-	 */
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+    /**
+     * Returns the redirected URL
+     * 
+     * @return
+     */
+    public String getRedirectedUrl() {
+        return redirectedUrl;
+    }
 
-	public Map<String, List<String>> getResponseHeaders() {
-		return responseHeaders;
-	}
+    /**
+     * Sets the redirected url
+     * 
+     * @param redirectedUrl
+     */
+    public void setRedirectedUrl(String redirectedUrl) {
+        this.redirectedUrl = redirectedUrl;
+    }
 
-	public void setResponseHeaders(Map<String, List<String>> responseHeaders) {
-		this.responseHeaders = responseHeaders;
-	}
+    /**
+     * Returns content type
+     * 
+     * @return
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * Sets the content type of the page
+     * 
+     * @param contentType
+     */
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    /**
+     * Returns the response headers of the web page
+     * 
+     * @return
+     */
+    public Map<String, List<String>> getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    /**
+     * Sets the response headers of that page..
+     * 
+     * @param responseHeaders
+     */
+    public void setResponseHeaders(Map<String, List<String>> responseHeaders) {
+        this.responseHeaders = responseHeaders;
+    }
 
 }
--- src/main/java/focusedCrawler/target/repository/WarcTargetRepository.java
+++ src/main/java/focusedCrawler/target/repository/WarcTargetRepository.java
@@ -1,30 +1,192 @@
 package focusedCrawler.target.repository;
 
-import org.archive.io.warc.WARCRecordInfo;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.zip.InflaterInputStream;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.archive.format.warc.WARCConstants;
+import org.archive.io.ArchiveRecord;
+import org.archive.io.warc.WARCReader;
+import org.archive.io.warc.WARCReaderFactory;
+import org.archive.io.warc.WARCRecord;
+import org.archive.io.warc.WARCWriter;
+import org.archive.io.warc.WARCWriterPoolSettingsData;
+import org.archive.uid.RecordIDGenerator;
+import org.archive.uid.UUIDGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import focusedCrawler.target.model.Page;
+import focusedCrawler.target.model.TargetModelWarcRecord;
 
 public class WarcTargetRepository implements TargetRepository {
 
-	private static final Logger logger = LoggerFactory.getLogger(WarcTargetRepository.class);
+    private static final Logger logger = LoggerFactory.getLogger(WarcTargetRepository.class);
 
-	private boolean compress = false;
+    private boolean compress = false;
+    private final Path directory;
+    private final long maxFileSize;
+    private WARCWriter writer;
+    private WARCReader reader;
 
-	@Override
-	public boolean insert(Page target) {
-		WARCRecordInfo warcRecord = new WARCRecordInfo();
-		warcRecord.setUrl(target.getURL().toString());
+    private RecordIDGenerator generator = new UUIDGenerator();
 
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public WarcTargetRepository(String directory) throws IOException {
+        this(Paths.get(directory), WARCConstants.DEFAULT_MAX_WARC_FILE_SIZE);
+    }
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
+    public WarcTargetRepository(String path, long maxFileSize) throws IOException {
+        this(Paths.get(path), maxFileSize);
+    }
 
-	}
+    public WarcTargetRepository(Path directory, long maxFileSize) throws IOException {
+        this.maxFileSize = maxFileSize;
+        if (!Files.exists(directory)) {
+            directory.toFile().mkdirs();
+        }
+        this.directory = directory;
+        File[] files = { directory.toFile() };
+        List<File> outputDirs = Arrays.asList(files);
+        this.writer = new WARCWriter(new AtomicInteger(),
+                new WARCWriterPoolSettingsData("crawl_data-", "${prefix}" + "-%d",
+                        WARCConstants.DEFAULT_MAX_WARC_FILE_SIZE, compress, outputDirs, null, generator));
+        //this.reader = WARCReaderFactory.get(new File("crawl_data")); 
+    }
 
+    @Override
+    public boolean insert(Page target) {
+        try {
+            TargetModelWarcRecord warcRecord = new TargetModelWarcRecord(target, generator.getRecordID());
+            writer.writeRecord(warcRecord);
+            return true;
+        } catch (IOException e) {
+            logger.debug(
+                    "Exception thrown while creating a " + "warc record with the following message. " + e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public void close() {
+        try{
+            writer.close();
+        }catch(IOException e){
+            logger.debug("Exception thrown when trying to close the WARC writer:"+e.getMessage());
+        }
+    }
+    
+//    /**
+//     * Returns Iterator of WarcReader
+//     * @return
+//     * @throws IOException
+//     */
+//    public Iterator<ArchiveRecord>  iterator() throws IOException{
+//        return reader.iterator();
+//    }
+//
+//    /**
+//     * Return file object of the current file written by WarcWriter
+//     * @return
+//     */
+//    private File getFile(){
+//        return writer.getFile();
+//    }
+//    
+//public class FilesIterator implements Iterator<String>, Closeable {
+//        
+//        private String next;
+//        private Iterator<Path> filesIt;
+//        private DirectoryStream<Path> filesStream;
+//        private BufferedReader linesReader;
+//
+//        public FilesIterator(Path directory) {
+//            try {
+//                filesStream = Files.newDirectoryStream(directory);
+//                filesIt = filesStream.iterator();
+//                if(filesIt.hasNext()) {
+//                    linesReader = openFile(filesIt.next());
+//                }
+//            } catch (IOException e) {
+//                throw new IllegalArgumentException(
+//                        "Failed to open target repository folder: "+directory, e);
+//            }
+//            this.next = readNext();
+//        }
+//
+//        private BufferedReader openFile(Path filePath) throws FileNotFoundException {
+//            return new BufferedReader(new InputStreamReader(new FileInputStream(filePath.toFile())));
+//        }
+//        
+//        private String readNext() {
+//            String nextLine = null;
+//            if(linesReader != null) {
+//                try {
+//                    nextLine = linesReader.readLine();
+//                } catch (IOException e) {
+//                    nextLine = null;
+//                }
+//                if(nextLine == null) { // end of file reached
+//                    IOUtils.closeQuietly(linesReader);
+//                    if(!filesIt.hasNext()) {
+//                        IOUtils.closeQuietly(filesStream);
+//                        return null; // no more file and lines available
+//                    }
+//                    // read next file available
+//                    Path filePath = null;
+//                    try {
+//                        filePath = filesIt.next();
+//                        linesReader = openFile(filePath);
+//                        nextLine = linesReader.readLine();
+//                    } catch (IOException e) {
+//                        String f = filePath == null ? null : filePath.toString();
+//                        throw new IllegalStateException("Failed to open file: "+f, e);
+//                    }
+//                }
+//            }
+//            return nextLine;
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            return this.next != null;
+//        }
+//
+//        @Override
+//        public String next() {
+//            if(this.next == null) {
+//                return null;
+//            } else {
+//                String returnValue = this.next;
+//                this.next = readNext();
+//                return returnValue;
+//            }
+//        }
+//        
+//        @Override
+//        public void remove() {
+//            throw new UnsupportedOperationException("remove");
+//        }
+//
+//        @Override
+//        public void close() {
+//            IOUtils.closeQuietly(linesReader);
+//            IOUtils.closeQuietly(filesStream);
+//        }
+//        
+//    }
+    
 }
