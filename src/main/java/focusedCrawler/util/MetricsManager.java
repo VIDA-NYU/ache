5ff738859195f613e73d3ecc6ad7a4a59e78ba92
Aecio Santos <aecio.solando@gmail.com>
2017-09-14 15:46:26 -0400

    Clean up code and close BufferedReader in MetricsManager.loadMetrics()

--- src/main/java/focusedCrawler/util/MetricsManager.java
+++ src/main/java/focusedCrawler/util/MetricsManager.java
@@ -20,156 +20,131 @@ import com.codahale.metrics.Timer;
 
 public class MetricsManager {
 
-	private final MetricRegistry metrics;
-	private ConsoleReporter reporter;
-	private String storageDirectory;
-	private static final Logger logger = LoggerFactory.getLogger(MetricsManager.class);
+    private static final Logger logger = LoggerFactory.getLogger(MetricsManager.class);
 
-	public MetricsManager() {
-		this(true, null);
-	}
+    private final MetricRegistry metrics;
+    private final String storageDirectory;
+    private ConsoleReporter reporter;
 
-	public MetricsManager(String directoryPath) {
-		this(true, directoryPath);
-	}
+    public MetricsManager() {
+        this(true, null);
+    }
 
-	public MetricsManager(boolean startConsoleReporter) {
-		this(startConsoleReporter, null);
-	}
+    public MetricsManager(String directoryPath) {
+        this(true, directoryPath);
+    }
 
-	public MetricsManager(boolean startConsoleReporter, String directoryPath) {
-		this(new MetricRegistry(), startConsoleReporter, directoryPath);
-	}
+    public MetricsManager(boolean startConsoleReporter) {
+        this(startConsoleReporter, null);
+    }
 
-	public MetricsManager(MetricRegistry metricsRegistry, boolean startConsoleReporter, String directoryPath) {
-		if (directoryPath != null) {
-			this.metrics = loadMetrics(metricsRegistry, directoryPath);
-		} else {
-			this.metrics = metricsRegistry;
-		}
-		this.storageDirectory = directoryPath;
-		if (startConsoleReporter) {
-			reporter = ConsoleReporter.forRegistry(metrics).convertRatesTo(TimeUnit.SECONDS)
-					.convertDurationsTo(TimeUnit.MILLISECONDS).build();
-			reporter.start(10, TimeUnit.SECONDS);
-		}
-	}
+    public MetricsManager(boolean startConsoleReporter, String directoryPath) {
+        this(new MetricRegistry(), startConsoleReporter, directoryPath);
+    }
 
-	public Timer getTimer(String name) {
-		return metrics.timer(name);
-	}
+    public MetricsManager(MetricRegistry metricsRegistry, boolean startConsoleReporter,
+                          String directoryPath) {
+        if (directoryPath != null) {
+            this.metrics = loadMetrics(metricsRegistry, directoryPath);
+        } else {
+            this.metrics = metricsRegistry;
+        }
+        this.storageDirectory = directoryPath;
+        if (startConsoleReporter) {
+            reporter = ConsoleReporter.forRegistry(metrics).convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS).build();
+            reporter.start(10, TimeUnit.SECONDS);
+        }
+    }
 
-	public Counter getCounter(String name) {
-		return metrics.counter(name);
-	}
+    public Timer getTimer(String name) {
+        return metrics.timer(name);
+    }
 
-	public void register(String name, Gauge<?> gauge) {
-		metrics.register(name, gauge);
-	}
+    public Counter getCounter(String name) {
+        return metrics.counter(name);
+    }
 
-	/**
-	 * Saves the metrics to a file for it to be reloaded when the crawler restarts
-	 * 
-	 * @param metricsRegistry
-	 * @param directoryPath
-	 * @throws IOException
-	 */
-	private void saveMetrics(MetricRegistry metricsRegistry, String directoryPath) {
-		String directoryName = directoryPath + "/metrics/";
-		File file = new File(directoryName);
-		if (file.exists()) {
-			file.delete();
-		}
-		file.mkdir();
-		try {
-			File outputFile = new File(directoryName + "metrics_counters.data");
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-			SortedMap<String, Counter> counters = metrics.getCounters();
-			for (String counter : counters.keySet()) {
-				Counter c = counters.get(counter);
-				writer.write(counter + ":" + c.getCount());
-				writer.newLine();
-				writer.flush();
-			}
-			writer.flush();
-			writer.close();
+    public void register(String name, Gauge<?> gauge) {
+        metrics.register(name, gauge);
+    }
 
-//			outputFile = new File(directoryName + "metrics_timers.data");
-//			writer = new BufferedWriter(new FileWriter(outputFile));
-//			SortedMap<String, Timer> timers = metrics.getTimers();
-//			for (String timer : timers.keySet()) {
-//				Timer t = timers.get(timer);
-//				writer.write(timer + ":" + t.getCount());
-//				writer.write("\n");
-//				writer.flush();
-//			}
-//			writer.flush();
-//			writer.close();
-		} catch (IOException e) {
-			logger.error("Unable to save metrics to a file." + e.getMessage());
-		}
+    /**
+     * Saves the metrics to a file for it to be reloaded when the crawler restarts
+     * 
+     * @param metricsRegistry
+     * @param directoryPath
+     * @throws IOException
+     */
+    private void saveMetrics(MetricRegistry metricsRegistry, String directoryPath) {
+        String directoryName = directoryPath + "/metrics/";
+        File file = new File(directoryName);
+        if (file.exists()) {
+            file.delete();
+        }
+        file.mkdir();
+        try {
+            File outputFile = new File(directoryName + "metrics_counters.data");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            SortedMap<String, Counter> counters = metrics.getCounters();
+            for (String counter : counters.keySet()) {
+                Counter c = counters.get(counter);
+                writer.write(counter + ":" + c.getCount());
+                writer.newLine();
+                writer.flush();
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            logger.error("Unable to save metrics to a file." + e.getMessage());
+        }
+    }
 
-	}
+    void saveMetrics(String directoryPath) {
+        this.saveMetrics(metrics, directoryPath);
+    }
 
-	void saveMetrics(String directoryPath) {
-		this.saveMetrics(metrics, directoryPath);
-	}
+    private void saveMetrics() {
+        this.saveMetrics(metrics, storageDirectory);
+    }
 
-	private void saveMetrics() {
-		this.saveMetrics(metrics, storageDirectory);
-	}
+    /**
+     * Loads metrics from the file at the directory path
+     * 
+     * @param metricsRegistry
+     * @param directoryPath
+     * @return
+     * @throws IOException
+     */
+    private MetricRegistry loadMetrics(MetricRegistry metricsRegistry, String directoryPath) {
+        File metricsFile = new File(directoryPath + "/metrics/metrics_counters.data");
+        if (metricsFile.exists()) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(metricsFile));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    String[] input = line.split(":");
+                    Counter counter = metricsRegistry.counter(input[0]);
+                    counter.inc(Integer.parseInt(input[1]));
+                }
+                reader.close();
+            } catch (Exception e) {
+                logger.error("Unable to deserialize counters : " + e.getMessage());
+            }
+        }
+        return metricsRegistry;
+    }
 
-	/**
-	 * Loads metrics from the file at the directry path
-	 * 
-	 * @param metricsRegistry
-	 * @param directoryPath
-	 * @return
-	 * @throws IOException
-	 */
-	private MetricRegistry loadMetrics(MetricRegistry metricsRegistry, String directoryPath) {
-		File metricsFile = new File(directoryPath + "/metrics/metrics_counters.data");
-		if (metricsFile.exists()) {
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(metricsFile));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					String[] input = line.split(":");
-					Counter counter = metricsRegistry.counter(input[0]);
-					counter.inc(Integer.parseInt(input[1]));
-				}
-			} catch (Exception e) {
-				logger.error("Unable to deserialize counters : " + e.getMessage());
-			}
-		}
+    public void close() {
+        if (reporter != null) {
+            reporter.report();
+            reporter.close();
+        }
+        saveMetrics();
+    }
 
-		// metricsFile = new File(directoryPath+"/metrics/metrics_timers.data");
-		// if(metricsFile.exists()) {
-		// try {
-		// BufferedReader reader = new BufferedReader(new FileReader(metricsFile));
-		// String line = null;
-		// SortedMap<String, Timer> timers = metrics.getTimers();
-		// while((line = reader.readLine()) != null) {
-		// String[] input = line.split(":");
-		// timers.get(input[0]);
-		// }
-		// }catch(Exception e) {
-		// logger.error("Unable to deserialize counters : "+ e.getMessage());
-		// }
-		// }
-		return metricsRegistry;
-	}
-
-	public void close() {
-		if (reporter != null) {
-			reporter.report();
-			reporter.close();
-		}
-		saveMetrics();
-	}
-
-	public MetricRegistry getMetricsRegistry() {
-		return metrics;
-	}
+    public MetricRegistry getMetricsRegistry() {
+        return metrics;
+    }
 
 }
