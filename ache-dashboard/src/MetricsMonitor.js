33294da1b2dae2bc5133092e2f421370474fdc2a
Aécio Santos <aecio.solando@gmail.com>
2017-12-01 19:00:14 -0500

    Display more metrics in crawler monitoring page

--- ache-dashboard/src/MetricsMonitor.js
+++ ache-dashboard/src/MetricsMonitor.js
@@ -177,43 +177,115 @@ class MetricsMonitor extends React.Component {
        this.state.metrics.gauges != null) {
       var metrics = this.state.metrics;
       // Create metrics table
-      table =
+      table = (
         <div className="row">
-          <div className="col-md-8 col-md-offset-2">
+          <div className="col-md-6">
             <div className="panel panel-default">
-              <div className="panel-heading"><b>Crawl Metrics</b></div>
-                <table className="table table-striped">
-                  <tbody>
-                    <tr>
-                      <td>Uncrawled Pages in Frontier</td>
-                      <td>{metrics['gauges']['frontier_manager.last_load.uncrawled'].value.toLocaleString()}</td>
-                    </tr>
-                    <tr>
-                      <td>Successfull Page Fetches (HTTP 2XX)</td>
-                      <td>{metrics['counters']['downloader.http_response.status.2xx'].count.toLocaleString()}</td>
-                    </tr>
-                    <tr>
-                      <td>Successfull Page Fetches</td>
-                      <td>{metrics['counters']['downloader.fetches.successes'].count.toLocaleString()}</td>
-                    </tr>
-                    <tr>
-                      <td>Page Fetch Errors </td>
-                      <td>{metrics['counters']['downloader.fetches.errors'].count.toLocaleString()}</td>
-                    </tr>
+              <div className="panel-heading"><b>General Metrics</b></div>
+              <table className="table table-striped">
+                <tbody>
+                  <tr>
+                    <td>Uncrawled Links in Frontier</td>
+                    <td>{metrics['gauges']['frontier_manager.last_load.uncrawled'].value.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Successfull Requests</td>
+                    <td>{metrics['counters']['downloader.fetches.successes'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Failed Requests</td>
+                    <td>{metrics['counters']['downloader.fetches.errors'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Aborted Requests</td>
+                    <td>{metrics['counters']['downloader.fetches.aborted'].count.toLocaleString()}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="panel panel-default">
+              <div className="panel-heading"><b>HTTP Response Metrics</b></div>
+              <table className="table table-striped">
+                <tbody>
+                  <tr>
+                    <td>HTTP 2XX: Success</td>
+                    <td>{metrics['counters']['downloader.http_response.status.2xx'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Error HTTP 401: Unauthorized</td>
+                    <td>{metrics['counters']['downloader.http_response.status.401'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Error HTTP 403: Forbidden</td>
+                    <td>{metrics['counters']['downloader.http_response.status.403'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Error HTTP 404: Not Found</td>
+                    <td>{metrics['counters']['downloader.http_response.status.404'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Error HTTP 5xx: Server Errors</td>
+                    <td>{metrics['counters']['downloader.http_response.status.5xx'].count.toLocaleString()}</td>
+                  </tr>
 
-                    <tr>
-                      <td>Fetch Time (mean) in secs </td>
-                      <td>{metrics['timers']['downloader.fetch.time'].mean.toFixed(3)}</td>
-                    </tr>
-                    <tr>
-                      <td>Fetch Time (p95) in secs </td>
-                      <td>{metrics['timers']['downloader.fetch.time'].p95.toFixed(3)}</td>
-                    </tr>
-                  </tbody>
-                </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="panel panel-default">
+              <div className="panel-heading"><b>Page Relevance</b></div>
+              <table className="table table-striped">
+                <tbody>
+                  <tr>
+                    <td>Total Pages</td>
+                    <td>{metrics['counters']['target.storage.pages.downloaded'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Relevant Pages</td>
+                    <td>{metrics['counters']['target.storage.pages.relevant'].count.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Irrelevant Pages</td>
+                    <td>{(metrics['counters']['target.storage.pages.downloaded'].count - metrics['counters']['target.storage.pages.relevant'].count).toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Harvest Rate</td>
+                    <td>{(metrics['gauges']['target.storage.harvest.rate'].value*100).toFixed(3)+'%'}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="panel panel-default">
+              <div className="panel-heading"><b>Performance</b></div>
+              <table className="table table-striped">
+                <tbody>
+                  <tr>
+                    <td>Page Fetch Time (Mean)</td>
+                    <td>{metrics['timers']['downloader.fetch.time'].mean.toFixed(2)+' ms'}</td>
+                  </tr>
+                  <tr>
+                    <td>Page Fetch Time (Percentile 75) </td>
+                    <td>{metrics['timers']['downloader.fetch.time'].p75.toFixed(2)+' ms'}</td>
+                  </tr>
+                  <tr>
+                    <td>Page Fetch Time (Percentile 95) </td>
+                    <td>{metrics['timers']['downloader.fetch.time'].p95.toFixed(2)+' ms'}</td>
+                  </tr>
+                  <tr>
+                    <td>Page Fetch Rate: Last 15 min (pages/sec) </td>
+                    <td>{metrics['timers']['downloader.fetch.time'].m15_rate.toFixed(2)+' pages/sec'}</td>
+                  </tr>
+                  <tr>
+                    <td>Page Fetch Rate: Last 5 min (pages/sec) </td>
+                    <td>{metrics['timers']['downloader.fetch.time'].m5_rate.toFixed(2)+' pages/sec'}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
+      )
 
       // Create time series charts
       for(var i = 0; i < this.charts.length; i++) {
@@ -234,8 +306,12 @@ class MetricsMonitor extends React.Component {
     return (
       <div className="row">
         <div className="col-md-12">
+          <h2>Monitoring</h2>
+          <h4><b>Crawler ID:</b> {this.crawlerId}</h4>
           {table}
+          <div className="row">
           {plots}
+          </div>
         </div>
       </div>
     );
