122937ae1f32258f3d818b4e8629973b4a4ffb07
Aécio Santos <aecio.solando@gmail.com>
2017-12-04 18:38:50 -0500

    Allow set start crawl parameters through URL query params

--- ache-dashboard/src/StartCrawler.js
+++ ache-dashboard/src/StartCrawler.js
@@ -5,14 +5,50 @@ import {api} from './RestApi';
 
 class StartCrawler extends React.Component {
 
+  DEFAULT_STATE = {
+    crawlerId: '',
+    crawlType: null,
+    seeds: null,
+    seedsContent: null,
+    modelFile: null,
+    invalidModel: null,
+    starting: false
+  };
+
   constructor(props) {
     super(props);
     this.messages = this.props.messages;
-    this.state = {};
+    this.state = Object.assign({}, this.DEFAULT_STATE);
+  }
+
+  cancelCrawl() {
+    this.setState(this.DEFAULT_STATE);
+    this.parseUrlQueryParameters();
   }
 
   componentWillUnmount() {
     this.messages.clearMessages();
+  }
+
+  componentWillMount() {
+    this.parseUrlQueryParameters();
+  }
+
+  parseUrlQueryParameters() {
+    if (this.props.location.search) {
+      const queryParams = this.props.location.search.substr(1).split('&');
+      for (let param of queryParams) {
+        let kv = param.split('=');
+        if(kv && kv.length === 2) {
+          if('crawlerId' === kv[0]) {
+            this.setState({crawlerId: kv[1], crawlerIdDisabled: true});
+          }
+          else if('crawlType' === kv[0]) {
+            this.setState({crawlType: kv[1], crawlTypeDisabled: true});
+          }
+        }
+      }
+    }
   }
 
   startCrawl(crawlerId) {
@@ -46,17 +82,6 @@ class StartCrawler extends React.Component {
         this.props.history.push('/')
       }
     }
-  }
-
-  cancelCrawl() {
-    this.setState({
-      crawlType: null,
-      seeds: null,
-      seedsContent: null,
-      modelFile: null,
-      invalidModel: null,
-      starting: false
-    });
   }
 
   handleSelectModelFile(e) {
@@ -101,7 +126,7 @@ class StartCrawler extends React.Component {
     reader.readAsText(file);
   }
 
-  setCrawlType(newCrawlType) {
+  handleCrawlTypeChange(newCrawlType) {
     this.setState({crawlType: newCrawlType});
   }
 
@@ -157,11 +182,14 @@ class StartCrawler extends React.Component {
 
             <div className="form-group">
               <label htmlFor="crawlerType">Select a Crawler Type:</label><br/>
-              <div className="btn-group" data-toggle="buttons" id="crawlerType" aria-label="Choose a crawler type">
-                <label className={'btn btn-primary ' + this.isSelected('DeepCrawl')} onClick={()=>this.setCrawlType('DeepCrawl')}>
+              <div className="btn-group" data-toggle="buttons" id="crawlerType"
+                aria-label="Choose a crawler type">
+                <label className={'btn btn-primary ' + this.isSelected('DeepCrawl')}
+                  onClick={()=>this.handleCrawlTypeChange('DeepCrawl')}>
                   <input type="radio" /> Deep Crawl
                 </label>
-                <label className={'btn btn-primary ' + this.isSelected('FocusedCrawl')} onClick={()=>this.setCrawlType('FocusedCrawl')}>
+                <label className={'btn btn-primary ' + this.isSelected('FocusedCrawl')}
+                  onClick={()=>this.handleCrawlTypeChange('FocusedCrawl')}>
                   <input type="radio" checked={this.isSelected('FocusedCrawl')}/> Focused Crawl
                 </label>
               </div>
@@ -170,36 +198,49 @@ class StartCrawler extends React.Component {
 
             <div className={'form-group' + (this.showErrorCrawlerId() ? ' has-error': '')} >
               <label htmlFor="crawlerId">Crawler ID:</label>
-              <input type="text" className="form-control" id="crawlerId" placeholder="my-crawler-name-1"
-                 onChange={(e)=>this.handleCrawlerIdChange(e)} style={{'maxWidth':'400px'}} />
+              <input type="text" className="form-control" id="crawlerId"
+                placeholder="my-crawler-name-1"
+                onChange={(e)=>this.handleCrawlerIdChange(e)}
+                style={{'maxWidth':'400px'}} value={this.state.crawlerId}
+                disabled={this.state.crawlerIdDisabled} />
             </div>
 
-            {
-              (this.state.crawlType === 'DeepCrawl')
-              &&
+            {(this.state.crawlType === 'DeepCrawl') &&
               <div>
                 <div className="form-group">
                   <label htmlFor="seedsInputFile">Seeds file:</label>
-                  <input type="file" className="form-control-file" id="seedsInputFile" aria-describedby="seedsFileHelp" onChange={(e)=>this.handleSelectSeedFile(e)} />
-                  <small id="seedsFileHelp" className="form-text text-muted">Please select a text file containing a list of URLs to start the crawl. The file should contain one URL per line, and each URL should start with "http://" or "https://".</small>
+                  <input type="file" className="form-control-file" id="seedsInputFile"
+                    aria-describedby="seedsFileHelp" onChange={(e)=>this.handleSelectSeedFile(e)} />
+                  <small id="seedsFileHelp" className="form-text text-muted">
+                    Please select a text file containing a list of URLs to start the crawl.
+                    The file should contain one URL per line, and each URL should start with "http://" or "https://".
+                  </small>
                 </div>
               </div>
             }
-            {
-              (this.state.crawlType === 'FocusedCrawl')
-              &&
+            {(this.state.crawlType === 'FocusedCrawl') &&
               <div className="form-group">
                 <label htmlFor="seedsInputFile">Model package:</label>
-                <input type="file" className="form-control-file" id="modelInputFile" aria-describedby="modelFileHelp" onChange={(e)=>this.handleSelectModelFile(e)} />
-                <small id="modelFileHelp" className="form-text text-muted">Please select the model file downloaded from DDT (&lt;domain-name&gt;_model.zip).</small>
+                <input type="file" className="form-control-file" id="modelInputFile"
+                  aria-describedby="modelFileHelp" onChange={(e)=>this.handleSelectModelFile(e)} />
+                <small id="modelFileHelp" className="form-text text-muted">
+                  Please select the model file downloaded from DDT (&lt;domain-name&gt;_model.zip).
+                </small>
                 { this.state.invalidModel && <p className="text-danger">Invalid model file. Select a zip file.</p>}
               </div>
             }
 
             <div className="form-group">
               <div className="btn-toolbar">
-                <button type="button" className="btn btn-default btn-md" onClick={()=>this.cancelCrawl()}><span className="glyphicon glyphicon-remove"></span>&nbsp;Cancel</button>
-                <button disabled={!enableStart} type="button" className="btn btn-primary btn-md" onClick={(e)=>this.startCrawl(this.state.crawlerId)}><span className="glyphicon glyphicon-play"></span>&nbsp;Start Crawl</button>
+                <button type="button" className="btn btn-default btn-md"
+                  onClick={()=>this.cancelCrawl()}>
+                  <span className="glyphicon glyphicon-remove"></span>&nbsp;Cancel
+                </button>
+                <button disabled={!enableStart} type="button"
+                  className="btn btn-primary btn-md"
+                  onClick={(e)=>this.startCrawl(this.state.crawlerId)}>
+                  <span className="glyphicon glyphicon-play"></span>&nbsp;Start Crawl
+                </button>
               </div>
               { isStarting && <small className="form-text text-muted"><br/>Starting crawler... Hang tight.</small> }
             </div>
